44dacfeab5d0a8c7bf16e03d450b7b11f3cd0e36
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2021-05-16 16:27:56 -0300

    algumas operacoes arrumadas

--- EXERCICIO1/main.c
+++ EXERCICIO1/main.c
@@ -8,56 +8,14 @@
 #include <stdlib.h>
 #include <float.h>
 #include <math.h>
+#include <fenv.h>
 
 #define DEBUG 
 
-/* Maior anterior ou igual número representavel */
-float 
-maiorAnterior(float n)
-{
-    return nextafterf(n, n-1);
-}
-
-/* Menos posterior ou igual número representavel */
-float 
-menorPosterior(float n)
-{
-    return nextafterf(n, n+1);
-}
+#pragma STDC FENV_ACCESS
 
 /**************************************************/
-/*                    OPERAÇÕES                   */
-/**************************************************/
-
-/* MÁXIMO DE 2 */
-/* MÁXIMO DE 4 */
-/* MÍNIMO DE 2 */
-/* MÍNIMO DE 4 */
-
-
-/* SOMA */
-intervalo_t
-soma(intervalo_t i1, intervalo_t i2)
-{
-    intervalo_t resultado;
-    resultado.min = i1.min + i2.min;
-    resultado.max = i1.max + i2.max;
-    return resultado;
-}
-
-/* SUBTRAÇÃO */
-intervalo_t
-subtacao(intervalo_t i1, intervalo_t i2)
-{
-    intervalo_t resultado;
-    resultado.min = i1.min - i2.max;
-    resultado.max = i1.max - i2.min;
-    return resultado;
-}
-
-/* MULTIPLICAÇÃO */
-/* DIVISÃO */
-
+/*                     STRUCS                     */
 /**************************************************/
 
 /* Representar um número como IEE754   */
@@ -81,6 +39,91 @@ conta
     char rot2[5];
 } conta_t;
 
+/**************************************************/
+/*                    OPERAÇÕES                   */
+/**************************************************/
+/* Maior anterior ou igual número representavel */
+float 
+maiorAnterior(float n)
+{
+    return nextafterf(n, n-1);
+}
+
+/* Menos posterior ou igual número representavel */
+float 
+menorPosterior(float n)
+{
+    return nextafterf(n, n+1);
+}
+
+/* MÍNIMO DE 4 */
+float
+min4(float n1, float n2, float n3, float n4)
+{
+    return fminf(fminf(n1,n2), fminf(n3,n4));
+}
+
+/* MÁXIMO DE 4 */
+float
+max4(float n1, float n2, float n3, float n4)
+{
+    return fmaxf(fmaxf(n1,n2), fmaxf(n3,n4));
+}
+
+/* SOMA */
+intervalo_t
+soma(intervalo_t i1, intervalo_t i2)
+{
+    intervalo_t resultado;
+    float somaMin, somaMax;
+
+    somaMin = i1.min + i2.min;
+    somaMax = i1.max + i2.max;
+    
+    resultado.min = maiorAnterior(somaMin);
+    resultado.max = menorPosterior(somaMax);
+
+    return resultado;
+}
+
+/* SUBTRAÇÃO */
+intervalo_t
+subtacao(intervalo_t i1, intervalo_t i2)
+{
+    intervalo_t resultado;
+    float subMin, subMax;
+
+    subMin = i1.min - i2.max;
+    subMax = i1.max - i2.min;
+
+    resultado.min = maiorAnterior(subMin);
+    resultado.max = menorPosterior(subMax);
+
+    return resultado;
+}
+
+/* MULTIPLICAÇÃO */
+intervalo_t
+multiplicacao(intervalo_t i1, intervalo_t i2)
+{
+    intervalo_t resultado;
+    float resMin, resMax;
+
+    resMin = min4(i1.min*i2.min, i1.min*i2.max, i1.max*i2.min, i1.max*i2.max);
+    resMax = max4(i1.min*i2.min, i1.min*i2.max, i1.max*i2.min, i1.max*i2.max);
+
+    resultado.min = maiorAnterior(resMin);
+    resultado.max = menorPosterior(resMax);
+    
+    return resultado;
+}
+
+/* DIVISÃO */
+/* TO-DO */
+
+/**************************************************/
+/*                      MAIN                      */
+/**************************************************/
 int
 main()
 {
@@ -91,7 +134,7 @@ main()
     conta_t *contas;
 
     /**************************************/
-    /* Le as entradas */
+    /* Le a quandiade de inputs e operações */
     scanf("%d\n%d", &nrInputs, &nrOperacoes);
 
     #ifdef DEBUG
@@ -109,6 +152,10 @@ main()
     /* Lê todas as entradas */
     for (int i = 0; i < nrInputs; i++)
         scanf("%f", &inputs[i]);
+
+    /* Lê todas as operações */
+    // for (int i = 0; i < nrOperacoes; i++)
+    //     scanf("%f", &contas[i]);
 
     /* Calcula os intervalos*/
     for (int i = 0; i < nrInputs; i++)
