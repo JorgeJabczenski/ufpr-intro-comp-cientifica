96413cec6d3c604db32fad14ba99de8b976a29c3
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2021-05-18 19:56:21 -0300

    continua no outro pc ai

--- EXERCICIO1/main.c
+++ EXERCICIO1/main.c
@@ -31,6 +31,7 @@ typedef struct
 typedef union
 {
     int32_t i;
+    // int sinal = 0b10000000000000000000000000000000 | i;
     float f;
     struct
     {
@@ -121,30 +122,49 @@ multiplicacao(intervalo_t i1, intervalo_t i2)
 }
 
 /* DIVISÃO */
-/* TO-DO 
-*/
 intervalo_t
 divisao(intervalo_t i1, intervalo_t i2)
 {
-    intervalo_t resultado, i2Inverso;
+    intervalo_t resultado;
 
     if (i2.min != 0 && i1.max != 0){
 
-        i2Inverso.min = maiorAnterior(1/i2.min);
-        i2Inverso.max = menorPosterior(1/i2.max);
-
-        resultado = multiplicacao(i1,i2Inverso);
+        resultado.min = maiorAnterior (min4(i1.min/i2.min, i1.min/i2.max, i1.max/i2.min, i1.max/i2.max));
+        resultado.max = menorPosterior(max4(i1.min/i2.min, i1.min/i2.max, i1.max/i2.min, i1.max/i2.max));
     }
 
     return resultado;
 
 }
 
-/* Confere se o intervalo é unitário */
+/* Confere se o intervalo é unitário (min == max) ou não (min < max) ou inválido (min > max)*/
 int
-isUnitario(intervalo_t intervao)
+isUnitario(intervalo_t intervalo)
 {
-    return 0;
+    Float_t min, max;
+    int sinalMin, sinalMax, ulps;
+
+    min.f = intervalo.min;
+    max.f = intervalo.max;
+
+    // printf("sinais---- -> %d %d\n" ,sinalMin, sinalMin);
+
+    // printf("min: %c | max: %c\n", (sinalMin)?'-':'+', (sinalMax)?'-':'+');
+    if (min.parts.sign != max.parts.sign) return 0;
+    
+    switch (min.parts.sign){
+        case 0:
+            printf("%d | %d\n", min.i, max.i);
+            if(min.i < max.i) return 0;
+        break;
+
+        case 1:
+            printf("negativo: %d | %d\n", min.i, max.i);
+            if (min.i > max.i) return 0;
+        break;
+    }
+
+    return 1;
 }
 
 /**************************************************/
@@ -174,7 +194,7 @@ main()
         float entrada;
         scanf("%*c %d %f\n",&indice, &entrada);
 
-        printf("---> %d | %1.8e\n", indice, entrada);
+        //printf("---> %d | %1.8e\n", indice, entrada);
 
         intervalos[indice-1].min = maiorAnterior(entrada);
         intervalos[indice-1].max = menorPosterior(entrada);
@@ -209,7 +229,7 @@ main()
             break;
         }
 
-        printf("%d = %d %c %d\n",destino, indice1, op, indice2);        
+        //printf("%d = %d %c %d\n",destino, indice1, op, indice2);        
         intervalos[destino-1] = resultado;
 
 
@@ -218,17 +238,17 @@ main()
     /* Imprime os intervalos */
     for(int i = 0; i < nrInputs + nrOperacoes; i++)
     {
-        printf("X%d = [\t% 1.8e,\t% 1.8e]\n",i+1, intervalos[i].min, intervalos[i].max);
+        printf("X%d = [     % 1.8e,      % 1.8e]\n",i+1, intervalos[i].min, intervalos[i].max);
     }
 
 
     /* Confere se as contas resultaram em intervalos unitários ou não */
-    printf("Não unitários\n");
+    printf("\nNão unitários:\n");
     for (int i = 0; i < nrOperacoes; i++)
     {
         if (!isUnitario(intervalos[nrInputs+i]))
         {
-            printf("X%d = [\t% 1.8e,\t% 1.8e]\n",nrInputs+i+1, intervalos[nrInputs+i].min, intervalos[nrInputs+i].max);
+            printf("X%d = [     % 1.8e,      % 1.8e]\n",nrInputs+i+1, intervalos[nrInputs+i].min, intervalos[nrInputs+i].max);
         }
     }
 
---
+++ EXERCICIO1/script.sh
@@ -0,0 +1,12 @@
+for i in {0..9}
+do 
+    cat testes/caso-0${i}.in | ./analiseIntervalar.out > out/my${i}.out
+done
+
+for i in {0..9}
+do
+    echo
+    echo "${i} ---------------------- ${i} ---------------------- ${i}"
+    echo
+    diff testes/caso-0${i}.out out/my${i}.out
+done
